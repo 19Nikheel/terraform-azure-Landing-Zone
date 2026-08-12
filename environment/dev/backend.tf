a6a64291a67f1f606756474e83a5cbf0c04a9ce8
Nikheel Kumar <flash.nik19@gmail.com>
2026-08-12 22:05:18 +0530

    backend block commented

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
 
-  backend "azurerm" {
-    resource_group_name  = "rg1"
-    storage_account_name = "storageaccountdev1910"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "rg1"
+  #   storage_account_name = "storageaccountdev1910"
+  #   container_name       = "tfstate"
+  #   key                  = "dev.terraform.tfstate"
+  # }
 }
